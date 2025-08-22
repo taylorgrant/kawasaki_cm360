f5bc357baf310e014c24b46f36c358cf5d80f36d
taylorgrant <taylorgrant38@gmail.com>
2025-08-22 21:58:35 +0000

    updating name from meta to CF

---
+++ run_gmail_cf_update.R
@@ -0,0 +1,7 @@
+source('/home/rstudio/R/kawasaki_cm360/R/gmail_update_cf.R')
+if (Sys.Date() > as.Date("2025-08-19")) {
+  gmail_update_cf(vehicle = "NAV")
+  gmail_update_cf(vehicle = "TeryxH2")
+} else {
+  gmail_update_cf(vehicle = "NAV")
+}
--- run_gmail_meta_update.R
+++
@@ -1,7 +0,0 @@
-source('/home/rstudio/R/kawasaki_cm360/R/gmail_update_meta.R')
-if (Sys.Date() > as.Date("2025-08-19")) {
-  gmail_update_meta(vehicle = "NAV")
-  gmail_update_meta(vehicle = "TeryxH2")
-} else {
-  gmail_update_meta(vehicle = "NAV")
-}
