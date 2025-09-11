affab9c34950cbdaef386a62d64714bb7cb6e073
taylorgrant <taylorgrant38@gmail.com>
2025-09-11 00:03:52 +0000

    gmail sales update should be live

--- R/gmail_update_sales.R
+++ R/gmail_update_sales.R
@@ -47,12 +47,12 @@ gmail_update_sales <- function(vehicle) {
   gmailr::gm_auth(token = token)
 
   # GMAIL - SEARCH FOR UNREAD EMAIL FROM KAWI WITH CSV ----------------------
-  # query <- glue(
-  #   'from:Brandon.Flanders@kmc-usa.com has:attachment is:unread filename:csv subject:"{vehicle_subject}"'
-  # )
-  query <- glue::glue(
-    'from:taylor_grant@gspsf.com has:attachment is:unread filename:csv subject:"{vehicle_subject}"'
+  query <- glue(
+    'from:Brandon.Flanders@kmc-usa.com has:attachment is:unread filename:csv subject:"{vehicle_subject}"'
   )
+  # query <- glue::glue(
+  #   'from:taylor_grant@gspsf.com has:attachment is:unread filename:csv subject:"{vehicle_subject}"'
+  # )
   msgs <- gmailr::gm_messages(search = query, num_results = 1)
 
   if (length(msgs[[1]]$messages) == 0) {
---
+++ run_gmail_sales_update.R
@@ -0,0 +1,2 @@
+source('/home/rstudio/R/kawasaki_cm360/R/gmail_update_sales.R')
+gmail_update_sales(vehicle = "NAV")
